a6520f6af088cda97ec836bed3360c8c7bf04578
Alejandro Alvarez <96664600+Alej0prepper@users.noreply.github.com>
2024-03-02 18:03:09 -0500

    try_parsing

--- Program.cs
+++ Program.cs
@@ -9,14 +9,23 @@ namespace Program{
         public static void Main(string[] args)
         {
             // using a common interface
-            Person person = new Person();
+            IPerson person = new Person();
             person.SetFirstName("Jorge");
             person.SetLastName("El Salvaje");
             // using a fluent interface
             IPersonFluent personFluent = new PersonFluent().SetFirstName("Jorge").SetLastName("El Curioso");
+            IPersonFluent Juan = new PersonFluent().SetFirstName("Juan").SetLastName("Perez");
+
+            try {var a = (PersonFluent)Juan;
+                a.GetID();
+            }
             
-            person.GetID();
-            // personFluent.GetID();
+            catch {System.Console.WriteLine("didn't work");}
+
+            
+            
+
+           
         }
     }
 }
